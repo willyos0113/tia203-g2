ab0ba48e9a94d177dc14406f7d2bc443d0b54116
Duncan <yaphong@outlook.com>
2025-04-12 16:43:34 +0800

    fgdgg

--- src/main/java/duncan.java
+++ src/main/java/duncan.java
@@ -1,6 +1,6 @@
 public class duncan {
 	public static void main(String[] args) {
 		System.out.println("要怎麼把大象放進冰箱？");
-		System.out.println("[Git] 使用fork 的好處- 想方涉法");
+		System.out.println("[Git] 使用fork 的好處- 想方涉asda法");
 	}
 }
